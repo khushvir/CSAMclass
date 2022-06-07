9607c82ae01561bdba198da5cd5fa13640c8712a
khushvir <khushvir.rishi@gmail.com>
2022-06-07 18:16:19 -0400

    in class activity

---
+++ webTech/nodejsClass/app.js
@@ -0,0 +1,13 @@
+var http =require("http");
+
+http.createServer(function(request, response){
+response.writeHead(200,{'Context-Type':'text/plain'})
+
+//send response body as hello world
+response.end('Hello World\n');
+
+
+}).listen(8081);
+
+
+console.log('Server running at http://127.0.0.1:8081/')
